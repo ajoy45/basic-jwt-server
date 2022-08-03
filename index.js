6e944bf09b39e7c204c6ff8b81574dc66c6511ee
ajoy biswas <ajoypust@gmail.comm>
2022-08-03 19:41:09 +0600

    order api add

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ozp7u.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -17,11 +17,32 @@ async function run() {
        await client.connect()
       console.log('db connected')
       const productCollection=client.db("products").collection('data');
+      const orderCollection=client.db('products').collection('order')
+      app.get('/products',async(req,res)=>{
+        const result=await productCollection.find({}).toArray()
+        res.send(result)
+      })
       app.post('/login',(req,res)=>{
         const email=req.body
         // console.log(email)
         const token = jwt.sign(email, process.env.ACCES_TOKEN);
         res.send({token})
+      })
+      app.post('/order',async(req,res)=>{
+        const productInfo=req.body;
+        console.log(productInfo)
+        const result=await orderCollection.insertOne(productInfo)
+        res.send(result)
+      })
+      app.get('/order',async(req,res)=>{
+        const orderProduct=await orderCollection.find({}).toArray()
+        res.send(orderProduct)
+      })
+      app.delete('/order/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:ObjectId(id)}
+        const result=await orderCollection.deleteOne(query)
+        res.send(result)
       })
       app.post('/upladepd',async(req,res)=>{
          const product=req.body;
